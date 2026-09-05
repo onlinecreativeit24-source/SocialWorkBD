c588d2a40288cbe2d61d605db5c2b84a6c9b3411
onlinecreativeit24-source <256857795+onlinecreativeit24-source@users.noreply.github.com>
2026-09-05 11:58:52 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,10 @@
+<!-- Firebase SDK -->
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
 
-// ==========================================
-// SocialWorkBD - Firebase Configuration
-// ==========================================
+<!-- Firebase Config -->
+<script src="firebase-config.js"></script>
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDsqRgRZTKZFvfu0r4UJc8Q5xlS7lBL41c",
-  authDomain: "socialworkbd-b1c00.firebaseapp.com",
-  projectId: "socialworkbd-b1c00",
-  storageBucket: "socialworkbd-b1c00.firebasestorage.app",
-  messagingSenderId: "999070456562",
-  appId: "1:999070456562:web:67101bb3148b157e67ce6b",
-  measurementId: "G-XVBRYV71BZ"
-};
-
-// Firebase initialize
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-// Firebase services
-const auth = firebase.auth();
-const db = firebase.firestore();
+<!-- App -->
+<script src="app.js"></script>
